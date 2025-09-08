8c6dce2cd73da223fd49cd5084c8f690571d5e07
Ethan Rouimi <contact@ethan-rouimi.com>
2025-09-08 22:34:26 +0200

    lsp ensure-installed

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "templ", "gopls", "tailwindcss", "pylsp", "ocamllsp", "nim_langserver" } -- ocaml
+local servers = { "templ", "gopls", "tailwindcss", "pylsp", "ocamllsp", "nim_langserver", "clangd" } -- ocaml
 
 local util = require "lspconfig/util"
 local nvlsp = require "nvchad.configs.lspconfig"
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,54 +20,70 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "lua-language-server", "stylua",
-        "html-lsp", "css-lsp" , "prettier",
-        "rust-analyzer", "gopls"
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "rust-analyzer",
+        "gopls",
+        "clangd",
+        "nimlangserver",
+        "python-lsp-server",
+        "svelte-language-server",
+        "tailwindcss-language-server",
+        "templ",
+        "typescript-language-server",
+        "vscode-solidity-server",
       },
     },
   },
 
-   "nvim-lua/plenary.nvim",
- { "nvim-tree/nvim-web-devicons", lazy = true },
+  "nvim-lua/plenary.nvim",
+  { "nvim-tree/nvim-web-devicons", lazy = true },
 
- {
-   "nvchad/ui",
+  {
+    "nvchad/ui",
     config = function()
       require "nvchad"
-    end
- },
+    end,
+  },
 
- {
+  {
     "nvchad/base46",
     lazy = true,
     build = function()
       require("base46").load_all_highlights()
     end,
- },
+  },
 
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "vim", "lua", "vimdoc",
-        "html", "css", "svelte"
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "svelte",
       },
     },
   },
   {
     "nvim-tree/nvim-tree.lua",
-    opts = require "configs.nvimtree"
+    opts = require "configs.nvimtree",
   },
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim" },
   },
   {
     "NvChad/nvim-colorizer.lua",
     event = "BufReadPre",
     opts = {
-     user_default_options = require "configs.colorizer"
-    }
+      user_default_options = require "configs.colorizer",
+    },
   },
 }
